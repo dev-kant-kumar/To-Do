2a1dc8af9c6d4e7c6b9a5edc873422e59277207e
Dev Kant Kumar <101362859+dev-kant-kumar@users.noreply.github.com>
2024-06-22 01:01:34 +0530

    Solid Form Validation Implemented

--- frontend/src/Components/SignUpForm.jsx
+++ frontend/src/Components/SignUpForm.jsx
@@ -4,12 +4,28 @@ import closeEye from "../assets/tabler--eye-off.png";
 import TodoIllustrationForSignUp from "../assets/TodoSignUp.png";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
+import { toast } from "react-toastify";
 import global from "../Components/Global";
+
 function SignUpForm() {
   const navigate = useNavigate();
 
   const [inputValue, setInputValue] = useState({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [haveError, setHaveError] = useState({
+    name: false,
+    username: false,
+    email: false,
+    password: false,
+    confirmPassword: false,
+  });
+  const [errorMessage, setErrorMessage] = useState({
     name: "",
     username: "",
     email: "",
@@ -35,14 +51,102 @@ function SignUpForm() {
     setInputValue((prevState) => ({ ...prevState, [name]: value }));
   };
 
-  function formValidation(e) {
+  const checkErrors = () => {
+    let errors = {};
+    let messages = {};
+
+    // Name validation
+    if (inputValue.name.length == 0) {
+      errors.name = true;
+      messages.name = "Please enter name!";
+    } else if (/[^a-zA-Z\s]/.test(inputValue.name)) {
+      errors.name = true;
+      messages.name = "Name can only have letters!";
+    } else if (inputValue.name.length < 3 || inputValue.name.length > 20) {
+      errors.name = true;
+      messages.name = "Name should be between 3 to 20 chars only!";
+    }
+
+    // Username validation
+    if (inputValue.username.length == 0) {
+      errors.username = true;
+      messages.username = "Please enter username!";
+    } else if (/[^a-zA-Z0-9]/.test(inputValue.username)) {
+      errors.username = true;
+      messages.username = "Username can only have letters and numbers!";
+    } else if (!isNaN(inputValue.username[0])) {
+      errors.username = true;
+      messages.username = "Username can't start with a number!";
+    } else if (
+      inputValue.username.length < 3 ||
+      inputValue.username.length > 15
+    ) {
+      errors.username = true;
+      messages.username = "Username should be between 3 to 15 chars only!";
+    }
+
+    // Email validation
+    const emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    if (inputValue.email.length == 0) {
+      errors.email = true;
+      messages.email = "Please enter email!";
+    } else if (!emailRegex.test(inputValue.email)) {
+      errors.email = true;
+      messages.email = "Invalid email format!";
+    }
+
+    // Password validation
+    const password = inputValue.password;
+    if (password.length == 0) {
+      errors.password = true;
+      messages.password = "Please enter password!";
+    } else if (!/[a-z]/.test(password)) {
+      errors.password = true;
+      messages.password =
+        "Password must contain at least one lowercase letter!";
+    } else if (!/[A-Z]/.test(password)) {
+      errors.password = true;
+      messages.password =
+        "Password must contain at least one uppercase letter!";
+    } else if (!/[0-9]/.test(password)) {
+      errors.password = true;
+      messages.password = "Password must contain at least one number!";
+    } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) {
+      errors.password = true;
+      messages.password =
+        "Password must contain at least one special character!";
+    } else if (password.length < 8 || password.length > 20) {
+      errors.password = true;
+      messages.password = "Password must be between 8 to 20 chars long!";
+    }
+    if (inputValue.confirmPassword.length == 0) {
+      errors.confirmPassword = true;
+      messages.confirmPassword = "Please enter confirm password!";
+    }
+
+    // Confirm password validation
+    if (inputValue.password !== inputValue.confirmPassword) {
+      errors.confirmPassword = true;
+      messages.confirmPassword = "Passwords do not match!";
+    }
+
+    return { errors, messages };
+  };
+
+  const formValidation = (e) => {
     e.preventDefault();
-    if (inputValue.password === inputValue.confirmPassword) {
+
+    const { errors, messages } = checkErrors();
+
+    setHaveError(errors);
+    setErrorMessage(messages);
+
+    if (Object.keys(errors).length === 0) {
       sendDataToBackend();
     } else {
-      toast.error("Passwords do not match");
+      toast.error("Please fill all the required fields");
     }
-  }
+  };
 
   const sendDataToBackend = async () => {
     await axios
@@ -88,10 +192,15 @@ function SignUpForm() {
           value={inputValue.name}
           placeholder="Name"
           onChange={handleFormInput}
-          required
         />
-        <p className="sigUp-form-validation-error-display-field">
-          {"name is required"}
+        <p
+          className={
+            haveError.name
+              ? "sigUp-form-validation-error-display-field-active"
+              : "sigUp-form-validation-error-display-field"
+          }
+        >
+          {errorMessage.name}
         </p>
 
         <input
@@ -101,10 +210,15 @@ function SignUpForm() {
           value={inputValue.username}
           placeholder="Username"
           onChange={handleFormInput}
-          required
         />
-        <p className="sigUp-form-validation-error-display-field">
-          {"name is required"}
+        <p
+          className={
+            haveError.username
+              ? "sigUp-form-validation-error-display-field-active"
+              : "sigUp-form-validation-error-display-field"
+          }
+        >
+          {errorMessage.username}
         </p>
 
         <input
@@ -114,10 +228,15 @@ function SignUpForm() {
           value={inputValue.email}
           placeholder="Email Id"
           onChange={handleFormInput}
-          required
         />
-        <p className="sigUp-form-validation-error-display-field">
-          {"name is required"}
+        <p
+          className={
+            haveError.email
+              ? "sigUp-form-validation-error-display-field-active"
+              : "sigUp-form-validation-error-display-field"
+          }
+        >
+          {errorMessage.email}
         </p>
 
         <div className="password-field">
@@ -127,7 +246,6 @@ function SignUpForm() {
             value={inputValue.password}
             placeholder="Password"
             onChange={handleFormInput}
-            required
           />
           <img
             src={showPassword ? openEye : closeEye}
@@ -136,8 +254,14 @@ function SignUpForm() {
             onClick={passwordHandler}
           />
         </div>
-        <p className="sigUp-form-validation-error-display-field">
-          {"name is required"}
+        <p
+          className={
+            haveError.password
+              ? "sigUp-form-validation-error-display-field-active"
+              : "sigUp-form-validation-error-display-field"
+          }
+        >
+          {errorMessage.password}
         </p>
 
         <div className="password-field">
@@ -147,7 +271,6 @@ function SignUpForm() {
             value={inputValue.confirmPassword}
             placeholder="Confirm Password"
             onChange={handleFormInput}
-            required
           />
           <img
             src={showConfirmPassword ? openEye : closeEye}
@@ -156,8 +279,14 @@ function SignUpForm() {
             onClick={confirmPasswordHandler}
           />
         </div>
-        <p className="sigUp-form-validation-error-display-field">
-          {"name is required"}
+        <p
+          className={
+            haveError.confirmPassword
+              ? "sigUp-form-validation-error-display-field-active"
+              : "sigUp-form-validation-error-display-field"
+          }
+        >
+          {errorMessage.confirmPassword}
         </p>
 
         <div id="terms-conditions">
