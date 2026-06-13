773182690871352827438d9a8f2e06a8b0cbca5c
dev-kant-kumar <dev.mytechhandle@gmail.com>
2026-06-13 16:02:19 +0530

    fix: register global Axios interceptor to attach authentication token to API calls

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,21 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./Store";
 import Authentication from "../src/Pages/AuthenticationPage.jsx";
+import axios from "axios";
+
+// Configure global Axios interceptor to automatically attach X-Authorization header
+axios.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers["X-Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
